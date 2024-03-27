527386a2b3df74f7fa50a23f80716bd2559883b0
Lais Urano <lais.urano@gmail.com>
2024-03-27 10:04:16 -0300

    Video1.5-Ajustando a Velocidade

---
+++ Assets/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    private InputManager inputManager;
+    [SerializeField] private float moveSpeed = 5;
+
+    void Start()
+    {
+        inputManager = new InputManager();
+    }
+
+    private void Update()
+    {
+        float moveDirection = inputManager.Movement * Time.deltaTime * moveSpeed;
+        transform.Translate(moveDirection, 0, 0);
+    }
+}
